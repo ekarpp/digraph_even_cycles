6906bdb4b2c15b2a5b4668a08c74aefa1119886f
ekarpp <eetu.karppinen@aalto.fi>
2022-07-30 17:31:24 +0300

    cleanup graphs

--- graphs/gen_topology_variance_exp_graphs.sh
+++ graphs/gen_topology_variance_exp_graphs.sh
@@ -5,8 +5,14 @@ DEGREE=6
 
 for v in 16 24 32 40 48 56 64
 do
-#    ./c_gen.py $v
-#    ./k_gen.py $v
+    ./c_gen.py $v
+    ./k_gen.py $v
     ./erdos_renyi.py $v $(( $v*($v-1) / 2 )) $NUM_GRAPHS
-#    ./config_model.py $v $DEGREE $NUM_GRAPHS
+    ./config_model.py $v $DEGREE $NUM_GRAPHS
 done
+
+mkdir -p topology_variance
+mv config/* topology_variance
+mv complete/* topology_variance
+mv cycle/* topology_variance
+mv erdos_renyi/* topology_variance
