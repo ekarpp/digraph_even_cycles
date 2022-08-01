b33fbc5a49edc0b7da8dedd9a83e353aa7e22014
ekarpp <eetu.karppinen@aalto.fi>
2022-08-01 11:46:34 +0300

    use multiplicative steps of cube root of 2 in edge test. use also erdos renyi graphs in edge test.

--- graphs/gen_edge_scale_exp_graphs.sh
+++ graphs/gen_edge_scale_exp_graphs.sh
@@ -3,6 +3,7 @@
 REPEATS=6
 VERTICES=40
 DEG=39
+EDGES=$(( $VERTICES*($VERTICES-1) ))
 FOLDER=edge_scalability
 
 mkdir -p $FOLDER
@@ -13,15 +14,18 @@ mkdir -p $FOLDER
 for i in {0..5}
 do
     cp k${VERTICES} $FOLDER/cm${VERTICES}_${DEG}_$i
+    cp k${VERTICES} $FOLDER/er${VERTICES}_${EDGES}_$i
 done
 
 rm k${VERTICES}
 
-DEG=$(echo "$DEG" | awk '{ print( int($1 / sqrt(sqrt(2))) ) }')
-
+DEG=$(echo "$DEG" | awk '{ print( int($1 / (2^(1/3)) ) ) }')
+EDGES=$(echo "$EDGES" | awk '{ print( int( $1 / (2^(1/3)) ) ) }')
 while [ $DEG -gt 0 ]
 do
-    echo $DEG
-    ./config_model.py $VERTICES $DEG $REPEATS $FOLDER yes
-    DEG=$(echo "$DEG" | awk '{ print( int($1 / sqrt(sqrt(2))) ) }')
+    echo "$DEG $EDGES"
+    ./config_model.py $VERTICES $DEG $REPEATS $FOLDER loops_n_dupes
+    ./erdos_renyi.py $VERTICES $EDGES $REPEATS $FOLDER
+    DEG=$(echo "$DEG" | awk '{ print( int($1 / (2^(1/3)) ) ) }')
+    EDGES=$(echo "$EDGES" | awk '{ print( int( $1 / (2^(1/3)) ) ) }')
 done
