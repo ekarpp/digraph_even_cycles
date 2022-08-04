a104153916501d4394b48cf6b2a4c15809030874
ekarpp <eetu.karppinen@aalto.fi>
2022-08-04 18:38:30 +0300

    fixes algo output processing script

--- scripts/process-algo-output.sh
+++ scripts/process-algo-output.sh
@@ -5,7 +5,11 @@ REPEATS=6
 for f in $(find ./results/*/ -type f -print)
 do
     echo $f
+    # everything except edge
     grep -A $(( $REPEATS*3 )) "VERTICES" $f | grep --line-buffered computed \
         | awk 'NR%6 != 1 { printf ("%s %s\n", $4, $7) }' > $f.plot
+    # edge
+    grep -A $(( $REPEATS*3 )) "degree" $f | awk \
+        'NR%19 == 1 { edges=$3*40 }
+         NR%19 == 0 || (NR%19 > 6 && NR%19%3 == 1) { printf ("%s %s\n", edges, $7) }' >> $f.plot
 done
-
