e1a33c44fcac8391c170324a73275daf5cfc57e8
ekarpp <eetu@karppinen.xyz>
2022-12-25 10:57:46 +0200

    inline wide_mul

--- src/gf.hh
+++ src/gf.hh
@@ -42,12 +42,101 @@ public:
     /* carryless multiplication of a and b, polynomial multiplicatoin that is
      * done with Intel CLMUL
      */
-    uint64_t clmul(const uint64_t a, const uint64_t b) const;
+    inline uint64_t clmul(const uint64_t a, const uint64_t b) const
+    {
+        const __m128i prod = _mm_clmulepi64_si128(
+            _mm_set_epi64x(0, a),
+            _mm_set_epi64x(0, b),
+            0x0
+            );
+
+        /* discard hi, only support up to 32 bit */
+        return _mm_extract_epi64(prod, 0x0);
+    }
 
     virtual uint64_t rem(const uint64_t a) const;
-    // :)
-    virtual __m256i wide_mul(const __m256i &a, const __m256i &b) const
-    { return _mm256_or_si256(a, b); }
+
+    /* multiply 8 bitsliced GF2_16 elements in one 256-bit vector.
+     * define here for inlining and avoiding overhead from virtual func. */
+    inline __m256i wide_mul(const __m256i &a, const __m256i &b) const
+    {
+        const __m128i prodlo = _mm_blend_epi32(
+            _mm_shuffle_epi32(
+                _mm_clmulepi64_si128(
+                    _mm256_extractf128_si256(a, 0),
+                    _mm256_extractf128_si256(b, 0),
+                    0x11
+                    ),
+                0x8D
+                ),
+            _mm_shuffle_epi32(
+                _mm_clmulepi64_si128(
+                    _mm256_extractf128_si256(a, 0),
+                    _mm256_extractf128_si256(b, 0),
+                    0x00
+                    ),
+                0xD8
+                ),
+            0x3
+            );
+
+        const __m128i prodhi = _mm_blend_epi32(
+            _mm_shuffle_epi32(
+                _mm_clmulepi64_si128(
+                    _mm256_extractf128_si256(a, 1),
+                    _mm256_extractf128_si256(b, 1),
+                    0x11
+                    ),
+                0x8D
+                ),
+            _mm_shuffle_epi32(
+                _mm_clmulepi64_si128(
+                    _mm256_extractf128_si256(a, 1),
+                    _mm256_extractf128_si256(b, 1),
+                    0x00
+                    ),
+                0xD8
+                ),
+            0x3
+            );
+
+        const __m256i prod = _mm256_set_m128i(prodhi, prodlo);
+
+        const __m256i lomask = _mm256_set1_epi32(0xFFFF);
+
+        const __m256i lo = _mm256_and_si256(
+            prod,
+            lomask
+            );
+        const __m256i hi = _mm256_srli_epi32(
+            prod,
+            16 // GF2_bits
+            );
+
+        const __m256i tmp = _mm256_xor_si256(
+            hi,
+            _mm256_xor_si256(
+                _mm256_srli_epi16(hi, 14),
+                _mm256_xor_si256(
+                    _mm256_srli_epi16(hi, 13),
+                    _mm256_srli_epi16(hi, 11)
+                    )
+                )
+            );
+
+        const __m256i rem = _mm256_xor_si256(
+            tmp,
+            _mm256_xor_si256(
+                _mm256_slli_epi16(tmp, 2),
+                _mm256_xor_si256(
+                    _mm256_slli_epi16(tmp, 3),
+                    _mm256_slli_epi16(tmp, 5)
+                    )
+                )
+            );
+
+        return _mm256_xor_si256(rem, lo);
+    }
 
     inline int get_n() const { return this->n; }
     inline uint64_t get_mod() const { return this->mod; }
@@ -58,8 +147,6 @@ class GF2_16 : public GF2_n
 {
 public:
     using GF2_n::GF2_n;
-
-    __m256i wide_mul(const __m256i &a, const __m256i &b) const override;
 
     uint64_t rem(const uint64_t a) const override;
 };
